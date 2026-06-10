0372d2023dd434650961929b1a9b415a70bc2eb9
Hi-Day <hidayaturrahman@binus.edu>
2026-06-10 10:28:33 +0700

    fix: override zero config with explicit builds to separate static and api

--- server.js
+++
@@ -1,7 +0,0 @@
-// Dummy entrypoint to satisfy Vercel's Node.js builder.
-// Vercel prioritizes the `public/` directory for static files,
-// so this file will only be invoked for unresolved routes (404s).
-module.exports = (req, res) => {
-  res.statusCode = 404;
-  res.end('Not found');
-};
